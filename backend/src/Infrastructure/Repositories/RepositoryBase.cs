3869d99088fde6db11065b86dd69202df70ce5ad
Kevin Treminio <kevintreminio5@gmail.com>
2023-11-22 11:05:49 -0600

    developer

---
+++ backend/src/Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,120 @@
+﻿using Ecommerce.Application.Persistence;
+using Ecommerce.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Ecommerce.Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : class
+    {
+        protected readonly EcommerceDbContext _dbContext;
+
+        public RepositoryBase(EcommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public void AddEntity(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+        }
+
+        public void AddRange(List<T> entities)
+        {
+            _dbContext.Set<T>().AddRange(entities);
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public void DeleteEntity(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public void DeleteRange(IReadOnlyList<T> entities)
+        {
+            _dbContext.Set<T>().RemoveRange(entities);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy, string? includeString, bool disableTracking = true)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (!string.IsNullOrWhiteSpace(includeString)) query = query.Include(includeString);
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null) return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, List<Expression<Func<T, object>>>? includes = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (includes != null) query = includes.Aggregate(query, (current, include) => current.Include(include));
+
+            if (predicate != null) query = query.Where(predicate);
+
+            if (orderBy != null) return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return (await _dbContext.Set<T>().FindAsync(id))!;
+        }
+
+        public async Task<T> GetEntityAsync(Expression<Func<T, bool>>? predicate, List<Expression<Func<T, object>>>? includes = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+
+            if (includes != null) query = includes.Aggregate(query, (current, include) => current.Include(include));
+
+            if (predicate != null) query = query.Where(predicate);
+
+            return (await query.FirstOrDefaultAsync())!;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public void UpdateEntity(T entity)
+        {
+            _dbContext.Set<T>().Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ backend/src/Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using Ecommerce.Application.Persistence;
+using Ecommerce.Infrastructure.Persistence;
+using System.Collections;
+
+namespace Ecommerce.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private Hashtable? _repositories;
+        private readonly EcommerceDbContext _dbContext;
+
+        public UnitOfWork(EcommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> Complete()
+        {
+            try
+            {
+                return await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en transaccion: ", ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        public IAsyncRepository<TEntity> Repository<TEntity>() where TEntity : class
+        {
+            if (_repositories is null)
+            {
+                _repositories = new Hashtable();
+            }
+
+            var type = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(RepositoryBase<>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _dbContext);
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IAsyncRepository<TEntity>)_repositories[type]!;
+        }
+    }
+}
